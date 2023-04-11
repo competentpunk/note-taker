d82bf223118ec4816c461612b48d9eff13ce079b
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-04-11 18:31:10 -0400

    Added server.js code

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // Create express server - see express exercises
+
+// define express
+
+const express = require('express');
+const PORT = 3001;
+const app = express();
+
+// define middleware (json, url encoded, static)
+json();
+
+
+// create / define routes 
+
+// start the server
+app.listen(PORT, ()=> console.log(`Listening at ${PORT}`));
